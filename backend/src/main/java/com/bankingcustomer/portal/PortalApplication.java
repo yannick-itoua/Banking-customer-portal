95c04fcb68d4ffb11bcc01a497561d2d1cf3ea9b
yannick-itoua <mangeyannick@gmail.com>
2025-10-06 15:02:06 -0600

    fix: Also exclude TomcatMetricsAutoConfiguration for complete container compatibility

--- backend/src/main/java/com/bankingcustomer/portal/PortalApplication.java
+++ backend/src/main/java/com/bankingcustomer/portal/PortalApplication.java
@@ -3,8 +3,12 @@ package com.bankingcustomer.portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.web.tomcat.TomcatMetricsAutoConfiguration;
 
-@SpringBootApplication(exclude = {SystemMetricsAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+    SystemMetricsAutoConfiguration.class,
+    TomcatMetricsAutoConfiguration.class
+})
 public class PortalApplication {
 
 	public static void main(String[] args) {
