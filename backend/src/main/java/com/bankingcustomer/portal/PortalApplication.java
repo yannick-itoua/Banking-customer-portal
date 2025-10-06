d4f2771cb07c3ac1289f8a075f27978fce54cd91
yannick-itoua <mangeyannick@gmail.com>
2025-10-06 14:48:58 -0600

    fix: Exclude SystemMetricsAutoConfiguration to prevent container metrics errors

--- backend/src/main/java/com/bankingcustomer/portal/PortalApplication.java
+++ backend/src/main/java/com/bankingcustomer/portal/PortalApplication.java
@@ -2,8 +2,9 @@ package com.bankingcustomer.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SystemMetricsAutoConfiguration.class})
 public class PortalApplication {
 
 	public static void main(String[] args) {
